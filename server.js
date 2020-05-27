4626731d3fea1acd10f511b8409c2d9064ea1c01
Bill Newman <william.newman@seattlecolleges.edu>
2020-05-27 01:47:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 app.use(express.static(__dirname + '/public')); // set location for static files
 app.use(require("body-parser").urlencoded({extended: true})); // parse form submissions
 
-app.set('views', path.join(__dirname, 'views')); //connection to views folder
+//app.set('views', path.join(__dirname, 'views')); //connection to views folder
 app.set('view engine', 'ejs'); // set the view engine to ejs
 
 // use res.render to load up an ejs view file
