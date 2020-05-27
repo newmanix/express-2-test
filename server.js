0114be7bf748503e05de30e7606aa9612e57041a
Bill Newman <william.newman@seattlecolleges.edu>
2020-05-26 19:19:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const PORT = 3000;
 //we can now serve up static pages - COMMENTED OUT FOR NOW
 //app.use(express.static(__dirname));
 
+//show JSON data at default URL
 app.get('/',(req,res) =>{
-  res.send(`We're at the root of the app, on port ${PORT}`);
+  //res.send(`We're at the root of the app, on port ${PORT}`);
+  res.json(data);
 });
+
 
 app.listen(PORT,()=>{
   console.log(`App is running on port ${PORT}`);
