c1d614b62d7b7473dd29106d9524251e3719367d
Bill Newman <william.newman@seattlecolleges.edu>
2020-05-26 19:14:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var data = require('./data/test.json');
 var app = express();
 const PORT = 3000;
 
-//we can now serve up static pages
-app.use(express.static(__dirname));
+//we can now serve up static pages - COMMENTED OUT FOR NOW
+//app.use(express.static(__dirname));
 
 app.get('/',(req,res) =>{
   res.send(`We're at the root of the app, on port ${PORT}`);
