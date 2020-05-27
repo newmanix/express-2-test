7bccf599a8506e5d37dcf0127340d62c7996e60f
Bill Newman <william.newman@seattlecolleges.edu>
2020-05-27 02:30:27 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express  = require('express');
 var data = require('./data/test.json');
 
 var app = express();
-const PORT = 3000;
+const PORT = 80;
 
 app.use(express.static(__dirname + '/public')); // set location for static files
 app.use(require("body-parser").urlencoded({extended: true})); // parse form submissions
